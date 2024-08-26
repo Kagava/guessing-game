9f050eccad14a4a5458e0a8baad9ecd912b8e8bc
Kagava <esspakov@mail.ru>
2024-08-26 21:17:04 +0700

    init: start guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
     constructor() {}
+   
 
     setRange(min, max) {
-
+        this.l = min;
+        this.r = max;
     }
 
     guess() {
-
+        return Math.ceil((this.l + this.r) / 2);
     }
 
     lower() {
-
+        this.r = this.guess();
     }
 
     greater() {
-
+        this.l = this.guess();
     }
 }
 
